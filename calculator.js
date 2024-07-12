9596e1d34a3127afb67538cb6cc9a3ba5c80b1b9
John Doe <149332198+kyungwan2023F@users.noreply.github.com>
2024-07-12 11:51:23 -0400

    added add subtract and multiply function to test them on browser console

---
+++ calculator.js
@@ -0,0 +1,11 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+  };
+  
+  const subtract = function(num1, num2) {
+      return num1 - num2;
+  }; 
+  
+  const multiply = function(num1, num2) {
+    return num1 * num2;
+  };
